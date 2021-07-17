46b808fdb5f6bcb430e00d53432b61268f3b17aa
Constantine <kon.rafikov@yandex.ru>
2021-07-18 01:03:22 +0300

    OG-174. Fixed an issue with missing 'gene' field

--- server.js
+++ server.js
@@ -9,26 +9,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);
-});
 
 app.post('/publication/all', (req, res) => {
   getGenesList((data) => {
     let geneSymbolsList = [];
     data.forEach((data) => {
-      geneSymbolsList.push(data.symbol);
+      geneSymbolsList.push(data.symbol.toLowerCase());
     });
 
     getPublicationsIdList(
@@ -49,6 +36,7 @@ app.post('/publication/all', (req, res) => {
               uid: article.uid,
               url: `https://www.ncbi.nlm.nih.gov/pubmed/${article.uid}`,
               title: article.title,
+              sortTitle: article.sorttitle,
               date: article.pubdate,
             });
           });
@@ -56,16 +44,22 @@ app.post('/publication/all', (req, res) => {
           // Check if gene symbol is mentioned in the title of an article
           const filteredFeed = feed.map(
             (article) => {
-              const geneSymbol = new RegExp(geneSymbolsList.join("|")).test(article.sorttitle);
-              if (geneSymbol.lastMatch !== null) {
+              const re = new RegExp(`${geneSymbolsList.join("|")}/gmi`);
+              const match = re.exec(article.sortTitle);
+              console.log(`/${geneSymbolsList.join("|")}/gmi`);
+
+              if (match !== null) {
                 return {
-                  gene: geneSymbol.lastMatch,
+                  gene: match[0],
                   ...article
                 };
               }
+
+              return article;
             }
           );
 
+          // console.log(filteredFeed);
           // console.log('query');
           res.json(filteredFeed);
         })
