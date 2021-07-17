a2fa22ea77d75d6ae13e47b285d93ae3ac4e9a9b
Constantine <kon.rafikov@yandex.ru>
2021-07-18 01:07:20 +0300

    OG-174. Returned headers

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
 app.post('/publication/all', (req, res) => {
   getGenesList((data) => {
     let geneSymbolsList = [];
@@ -61,6 +60,18 @@ app.post('/publication/all', (req, res) => {
 
           // console.log(filteredFeed);
           // console.log('query');
+          // Website you wish to allow to connect
+          res.header('Access-Control-Allow-Origin', '*');
+
+          // Request methods you wish to allow
+          res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+          // Request headers you wish to allow
+          res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+          // Set to true if you need the website to include cookies in the requests sent
+          // to the API (e.g. in case you use sessions)
+          res.header('Access-Control-Allow-Credentials', true);
           res.json(filteredFeed);
         })
     })
