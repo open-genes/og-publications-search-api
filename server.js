9bba31168bbfdcc5e5475b0b0770cb097e5e1004
Constantine <kon.rafikov@yandex.ru>
2021-07-17 23:16:07 +0300

    OG-174. Set headers to express instance

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header('Access-Control-Allow-Credentials', true);
+});
 
 app.post('/publication/all', (req, res) => {
   getGenesList((data) => {
@@ -53,18 +67,6 @@ app.post('/publication/all', (req, res) => {
           );
 
           // console.log('query');
-          // Website you wish to allow to connect
-          res.header('Access-Control-Allow-Origin', '*');
-
-          // Request methods you wish to allow
-          res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-          // Request headers you wish to allow
-          res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-          // Set to true if you need the website to include cookies in the requests sent
-          // to the API (e.g. in case you use sessions)
-          res.header('Access-Control-Allow-Credentials', true);
           res.json(filteredFeed);
         })
     })
