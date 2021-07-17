da47adefc76314fa57eb0fdcfc5e7936ac0005d5
Constantine <kon.rafikov@yandex.ru>
2021-07-17 23:01:17 +0300

    OG-174. CORS errors fix

--- server.js
+++ server.js
@@ -53,7 +53,18 @@ app.post('/publication/all', (req, res) => {
           );
 
           // console.log('query');
-          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          // Website you wish to allow to connect
+          res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+          // Request methods you wish to allow
+          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+          // Request headers you wish to allow
+          res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+          // Set to true if you need the website to include cookies in the requests sent
+          // to the API (e.g. in case you use sessions)
+          res.setHeader('Access-Control-Allow-Credentials', true);
           res.json(filteredFeed);
         })
     })
