9b3850f81459ddc96a147582c4b80156bc1f478a
Constantine <kon.rafikov@yandex.ru>
2021-07-26 19:59:07 +0300

    Simple error handling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import {getGenesList} from "./loaders/open-genes-api";
 import {getPublicationsIdList, getPublicationsInIdsList} from "./loaders/pubmed-api";
+import {errorLogger} from "./utils/error-logger";
 
 const express = require('express');
 const app = express();
@@ -16,6 +17,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Catch bodyParser errors occurring while parsing invalid JSON in requests
+app.use((error, req, res, next) => {
+  if (error instanceof SyntaxError) {
+    errorLogger(error, 'Express bodyParser error');
+    res.sendStatus(500);
+  } else {
+    next();
+  }
+});
+
 // Configure cache middleware
 let memCache = new cache.Cache();
 let cacheMiddleware = (duration) => {
@@ -23,15 +34,15 @@ let cacheMiddleware = (duration) => {
     const currentDate = (new Date()).valueOf().toString();
     const random = Math.random().toString();
     const hash = crypto.createHash('md5').update(`${currentDate}${random}`).digest("hex");
-    let key =  `__search__${hash}`;
+    let key = `__search__${hash}`;
     let cacheContent = memCache.get(key);
-    if(cacheContent){
-      res.send( cacheContent );
+    if (cacheContent) {
+      res.send(cacheContent);
       return
-    }else{
+    } else {
       res.sendResponse = res.send
       res.send = (body) => {
-        memCache.put(key,body,duration * 1000);
+        memCache.put(key, body, duration * 1000);
         res.sendResponse(body)
       }
       next()
@@ -43,6 +54,14 @@ let cacheMiddleware = (duration) => {
 // TODO: transfer all the logic into the module
 app.post('/publication/all', cacheMiddleware(30), (req, res) => {
   if (Object.keys(req.body).length !== 0) {
+    const portion = 10;
+    const limit = req.body.limit !== undefined
+      ? req.body.limit
+      : portion;
+    const page = req.body.page !== undefined
+      ? Math.ceil(req.body.page)
+      : 1;
+
     getGenesList((data) => {
       let geneSymbolsList = [];
       data.forEach((data) => {
@@ -52,10 +71,9 @@ app.post('/publication/all', cacheMiddleware(30), (req, res) => {
         req.body.symbols !== undefined
           ? req.body.symbols :
           geneSymbolsList,
-        req.body.limit !== undefined
-          ? req.body.limit
-          : 0,
+        limit,
         (data) => {
+          console.log(limit, page);
           const publicationIdsList = data;
 
           getPublicationsInIdsList(
@@ -63,85 +81,84 @@ app.post('/publication/all', cacheMiddleware(30), (req, res) => {
             (data) => {
               // Form an article object and a feed list
               const feed = [];
-              Object.values(data.result).forEach((article) => {
-                /* structure:
-                  [
-                    { ...article },
-                    { ...article },
-                    [ '33733001', '33650673' ]
-                  ]
-                */
 
-                if (article.hasOwnProperty('uid')) {
-                  feed.push({
-                    uid: article.uid,
-                    url: `https://www.ncbi.nlm.nih.gov/pubmed/${article.uid}`,
-                    title: article.title,
-                    sortTitle: article.sorttitle,
-                    date: new Date(article.pubdate).getTime(),
-                  });
-                }
-              });
-
-              // Check if gene symbol is mentioned in the title of an article.
-              // Exclude them from response if not and add a gene symbol to response.
-              const re = new RegExp(`${geneSymbolsList.join("|")}/gmi`);
-
-              const filteredFeed = feed.filter(
-                (article) => {
-                  if (re.exec(article.sortTitle) !== null) {
-                    return article
-                  }
-                }).map(
-                (article) => {
-                  const match = re.exec(article.sortTitle);
-                  if (match !== null) {
-                    return {
-                      gene: match[0],
-                      ...article
-                    };
-                  }
-                }
-              );
-
-              console.log(filteredFeed);
-
-              const page = req.body.page !== undefined
-                ? Math.ceil(req.body.page)
-                : 1;
-              const portion = 10;
-              const startIndex = Math.ceil((page - 1) * portion);
-              const endIndex = startIndex + portion;
-
-              try {
-                const result = {
-                  total: filteredFeed.length,
-                  page: page,
-                  items: [...filteredFeed.slice(startIndex, endIndex)],
-                };
-                res.json(result);
-
-                console.log(
-                  `\n--- New search --- \n`,
-                  `total: ${filteredFeed.length} \n`,
-                  `symbols:${req.body.symbols} \n`,
-                  `limit: ${req.body.limit} \n`,
-                  `page: ${req.body.page} of ${Math.ceil(filteredFeed.length / portion)}`
-                );
-              } catch (err) {
-                console.log(err);
+              // TODO: handle error in getPublicationsInIdsList when Esummary API returns esummaryresult: [ 'Empty id list - nothing todo' ]
+              if (data.result === undefined) {
                 res.sendStatus(500);
+              } else {
+                Object.values(data.result).forEach((article) => {
+                  /* structure:
+                    [
+                      { ...article },
+                      { ...article },
+                      [ '33733001', '33650673' ]
+                    ]
+                  */
+
+                  if (article.hasOwnProperty('uid')) {
+                    feed.push({
+                      uid: article.uid,
+                      url: `https://www.ncbi.nlm.nih.gov/pubmed/${article.uid}`,
+                      title: article.title,
+                      sortTitle: article.sorttitle,
+                      date: new Date(article.pubdate).getTime(),
+                    });
+                  }
+                });
+
+                // Check if gene symbol is mentioned in the title of an article.
+                // Exclude them from response if not and add a gene symbol to response.
+                const re = new RegExp(`${geneSymbolsList.join("|")}/gmi`);
+
+                const filteredFeed = feed.filter(
+                  (article) => {
+                    if (re.exec(article.sortTitle) !== null) {
+                      return article
+                    }
+                  }).map(
+                  (article) => {
+                    const match = re.exec(article.sortTitle);
+                    if (match !== null) {
+                      return {
+                        gene: match[0],
+                        ...article
+                      };
+                    }
+                  }
+                );
+
+                const startIndex = Math.ceil((page - 1) * portion);
+                const endIndex = startIndex + portion;
+
+                try {
+                  const result = {
+                    total: filteredFeed.length,
+                    page: page,
+                    items: [...filteredFeed.slice(startIndex, endIndex)],
+                  };
+                  res.json(result);
+
+                  console.log(
+                    `\n--- New search --- \n`,
+                    `total: ${filteredFeed.length} \n`,
+                    `symbols:${req.body.symbols} \n`,
+                    `limit: ${limit} \n`,
+                    `page: ${page} of ${Math.ceil(filteredFeed.length / portion)}`
+                  );
+                } catch (err) {
+                  errorLogger(err);
+                  res.sendStatus(500);
+                }
               }
             })
         })
     });
   } else {
     console.log(req.body);
-    res.sendStatus(500);
+    errorLogger(500);
   }
 });
 
-// Runtime logs
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- utils/error-logger.js
+++ utils/error-logger.js
@@ -1,14 +1,14 @@
 // TODO: error handling
 
-export function errorLogger(error) {
+export function errorLogger(error, message='') {
   if (error.response) {
     // client received an error response (5xx, 4xx)
-    console.error(error);
+    console.error(error, message);
   } else if (error.request) {
     // client never received a response, or request never left
-    console.error(error);
+    console.error(error, message);
   } else {
     // anything else
-    console.error(error);
+    console.error(error, message);
   }
 }
